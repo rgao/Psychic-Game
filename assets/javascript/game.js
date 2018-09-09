2945c2684b6be58024bfa0d8871903cd707dc636
rgao <xinghoshi@gmail.com>
2018-09-08 18:22:45 -0700

    fixed unresponsive bug, made ten other responsive bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 var n_wins = 0;
 var n_losses = 0;
 var n_guesses = 9;
-var guessed_letters = [];
+var guessed_letters = [9];
 
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -19,22 +19,22 @@ document.onkeyup = function(event) {
     // } else 
     if (event.key === computerGuess) {
         n_wins += 1;
-        document.getElementById("wins").innerHTML = n_wins;
+        document.getElementById("wins").textContent = n_wins;
         computerGuess = alphabet[Math.floor(Math.random() * 26)];
     } else {
-        for (i in guessed_letters) {
-            if (event.key === i) {
+        for (j = 0; j < guessed_letters.length ; j++) {
+            if (event.key === guessed_letters[j]) {
                 alert("You have already guessed this letter. Try something else.");
             } else {
                 n_guesses -= 1;
-                document.getElementById("guesses").innerHTML = n_guesses;
+                document.getElementById("guesses").textContent = n_guesses;
                 if (n_guesses === 0) {
                     n_losses += 1;
-                    document.getElementById("losses").innerHTML = n_losses;
+                    document.getElementById("losses").textContent = n_losses;
                     computerGuess = alphabet[Math.floor(Math.random() * 26)];
                 } else {
                     guessed_letters.push(event.key);
-                    document.getElementById("already_guessed").innerHTML = guessed_letters;
+                    document.getElementById("already_guessed").textContent = guessed_letters;
                 };
             };
         };
